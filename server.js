5d24f68dd0f6b12d35d0d2f88fa7e5468a4f96c1
camilo camayo <acamilo0318@gmail.com>
2023-04-16 16:30:47 -0500

    API CRUD OK

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const api = require('./api');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
+
+
+app.use(bodyParser.json());
 
 app.listen(port, function () {
     console.log("Server is listening at port: " + port);
@@ -9,3 +14,6 @@ app.listen(port, function () {
 app.get('/', function (req, res) {
     res.send("hello world");
 });
+
+app.use('/api', api);
+
